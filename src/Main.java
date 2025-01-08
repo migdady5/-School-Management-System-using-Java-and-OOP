f260b47d9855f42a089462ac3bf902a2ec8ffbe4
migdady05 <chatgbtacc62@gmail.com>
2025-01-08 05:00:27 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Ali", 20, "Amman", "S123");
-        Student student2 = new Student("Sara", 22, "Irbid", "S124");
+        Student student2 = new Student("ali", 22, "Irbid", "S124");
         Teacher teacher1 = new Teacher("Mr. John", 40, "Amman", "Math", "T001");
         Teacher teacher2 = new Teacher("Ms. Emily", 35, "Zarqa", "English", "T002");
 
